ebe04c5c82cca4e2ae6f7c6b15b4839b3e8c30b2
jf-tayiba <jannatulferdoustayiba@gmail.com>
2026-03-14 06:22:45 +0100

    Third commit

--- Reservation.java
+++ Reservation.java
@@ -2,32 +2,69 @@ import java.io.Serializable;
 
 public class Reservation implements Serializable {
 
-    private int reservationId;
-    private int customerId;
-    private int roomId;
+    private int id;
+    private String name;
+    private String gender;
+    private String email;
+    private String phone;
+    private String city;
+    private String country;
+    private String nid;
+    private String consent;
+    private int roomNo;
+    private String roomType;
+    private double price;
     private String checkIn;
     private String checkOut;
 
-    public Reservation(int reservationId, int customerId, int roomId, String checkIn, String checkOut) {
-        this.reservationId = reservationId;
-        this.customerId = customerId;
-        this.roomId = roomId;
-        this.checkIn = checkIn;
-        this.checkOut = checkOut;
+    public Reservation(int id,String name,String gender,String email,String phone,
+                        String city,String country,String nid,String consent,
+                        int roomNo,String roomType,double price,
+                        String checkIn,String checkOut){
+
+        this.id=id;
+        this.name=name;
+        this.gender=gender;
+        this.email=email;
+        this.phone=phone;
+        this.city=city;
+        this.country=country;
+        this.nid=nid;
+        this.consent=consent;
+        this.roomNo=roomNo;
+        this.roomType=roomType;
+        this.price=price;
+        this.checkIn=checkIn;
+        this.checkOut=checkOut;
     }
 
-    public int getReservationId() { return reservationId; }
-    public void setReservationId(int reservationId) { this.reservationId = reservationId; }
+    public int getId()
+        { return id; }
+    public String getName()
+        { return name; }
+    public String getGender()
+        { return gender; }
+    public String getEmail()
+        { return email; }
+    public String getPhone()
+        { return phone; }
+    public String getCity()
+        { return city; }
+    public String getCountry()
+        { return country; }
+    public String getNid()
+        { return nid; }
+    public String getConsent()
+        { return consent; }
+    public int getRoomNo()
+        { return roomNo; }
+    public String getRoomType()
+        { return roomType; }
+    public double getPrice()
+        { return price; }
+    public String getCheckIn()
+        { return checkIn; }
+    public String getCheckOut()
+        { return checkOut; }
 
-    public int getCustomerId() { return customerId; }
-    public void setCustomerId(int customerId) { this.customerId = customerId; }
-
-    public int getRoomId() { return roomId; }
-    public void setRoomId(int roomId) { this.roomId = roomId; }
-
-    public String getCheckIn() { return checkIn; }
-    public void setCheckIn(String checkIn) { this.checkIn = checkIn; }
-
-    public String getCheckOut() { return checkOut; }
-    public void setCheckOut(String checkOut) { this.checkOut = checkOut; }
 }
