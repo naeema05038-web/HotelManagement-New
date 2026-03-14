cc331f8121b032ca7091f2a9cb338edb746f4fd0
jf-tayiba <jannatulferdoustayiba@gmail.com>
2026-03-14 23:50:48 +0600

    second commit

--- Room.java
+++ Room.java
@@ -2,7 +2,6 @@ import java.io.Serializable;
 
 public class Room implements Serializable {
 
-   
     private int roomNo;
     private RoomType type;
     private double price;
@@ -21,38 +20,30 @@ public class Room implements Serializable {
         this.available = available;
     }
 
-    public int getRoomNo()
-        { return roomNo; }
-    public void setRoomNo(int roomNo)
-        { this.roomNo = roomNo; }
+    
+    public int getRoomNo() { return roomNo; }
+    public void setRoomNo(int roomNo) { this.roomNo = roomNo; }
 
-    public RoomType getType()
-        { return type; }
-    public void setType(RoomType type)
-        { this.type = type; }
+    public RoomType getType() { return type; }
+    public void setType(RoomType type) { this.type = type; }
 
-    public double getPrice()
-        { return price; }
-    public void setPrice(double price)
-        { this.price = price; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 
-    public int getFloor()
-            { return floor; }
-    public void setFloor(int floor)
-            { this.floor = floor; }
+    public int getFloor() { return floor; }
+    public void setFloor(int floor) { this.floor = floor; }
 
-    public int getCapacity()
-        { return capacity; }
-    public void setCapacity(int capacity)
-        { this.capacity = capacity; }
+    public int getCapacity() { return capacity; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
 
-    public String getCleanStatus()
-        { return cleanStatus; }
-    public void setCleanStatus(String cleanStatus)
-        { this.cleanStatus = cleanStatus; }
+    public String getCleanStatus() { return cleanStatus; }
+    public void setCleanStatus(String cleanStatus) { this.cleanStatus = cleanStatus; }
 
-    public boolean isAvailable()
-            { return available; }
-    public void setAvailable(boolean available)
-            { this.available = available; }
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+
+    @Override
+    public String toString() {
+        return "Room " + roomNo + " (" + type + ")";
+    }
 }
