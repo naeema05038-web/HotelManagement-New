0367d6c20536d1f5fb81fa0b204a0c13940bc50a
jf-tayiba <jannatulferdoustayiba@gmail.com>
2026-03-14 07:10:50 +0100

    Third commit

--- ReservationManager.java
+++ ReservationManager.java
@@ -1,58 +1,54 @@
+import java.io.*;
 import java.util.*;
 
-public class ReservationManager {
+public class ReservationManager implements Serializable {
 
     private static ReservationManager instance;
     private List<Reservation> reservations;
 
-    private ReservationManager(){
-        reservations = new ArrayList<>();
+    private ReservationManager() {
+        loadFromFile();
+        if (reservations == null) reservations = new ArrayList<>();
     }
 
-    public static ReservationManager getInstance(){
-
-        if(instance==null){
-            instance = new ReservationManager();
-        }
-
+    public static ReservationManager getInstance() {
+        if (instance == null) instance = new ReservationManager();
         return instance;
     }
 
-    public void addReservation(Reservation r){
+    public void addReservation(Reservation r) {
         reservations.add(r);
+        saveToFile();
     }
 
-    public void deleteReservation(int index){
+    public void updateReservation(int index, Reservation r) {
+        reservations.set(index, r);
+        saveToFile();
+    }
+
+    public void deleteReservation(int index) {
         reservations.remove(index);
+        saveToFile();
     }
 
-    public void updateReservation(int index, Reservation r){
-        reservations.set(index,r);
-    }
-
-    public List<Reservation> getAllReservations(){
+    public List<Reservation> getAllReservations() {
         return reservations;
     }
 
-
-    public interface SearchStrategy{
-        boolean match(Reservation r,String key);
-    }
-
-
-    public List<Reservation> search(String key, SearchStrategy strategy){
-
-        List<Reservation> result = new ArrayList<>();
-
-        for(Reservation r:reservations){
-
-            if(strategy.match(r,key)){
-                result.add(r);
-            }
-
+    private void saveToFile() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("reservations.dat"))) {
+            oos.writeObject(reservations);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        return result;
     }
 
+    @SuppressWarnings("unchecked")
+    private void loadFromFile() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("reservations.dat"))) {
+            reservations = (List<Reservation>) ois.readObject();
+        } catch (Exception e) {
+            reservations = new ArrayList<>();
+        }
+    }
 }
