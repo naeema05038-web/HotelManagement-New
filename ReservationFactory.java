ba62fce3406b22df8a4ddd5b303696019dbb4689
jf-tayiba <jannatulferdoustayiba@gmail.com>
2026-03-14 06:23:33 +0100

    Third commit

--- ReservationFactory.java
+++ ReservationFactory.java
@@ -1,13 +1,28 @@
 public class ReservationFactory {
 
     public static Reservation createReservation(
-            int reservationId,
-            int customerId,
-            int roomId,
+            int id,
+            String name,
+            String gender,
+            String email,
+            String phone,
+            String city,
+            String country,
+            String nid,
+            String consent,
+            int roomNo,
+            String roomType,
+            double price,
             String checkIn,
-            String checkOut) {
+            String checkOut
+    ){
 
-        return new Reservation(reservationId, customerId, roomId, checkIn, checkOut);
+        return new Reservation(
+                id,name,gender,email,phone,
+                city,country,nid,consent,
+                roomNo,roomType,price,
+                checkIn,checkOut
+        );
     }
 
 }
