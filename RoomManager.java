4c3f1d9737e4945d82a8e3edae020d72cfc8f86f
jf-tayiba <jannatulferdoustayiba@gmail.com>
2026-03-18 06:27:55 +0600

    second commit

--- RoomManager.java
+++ RoomManager.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class RoomManager implements Serializable {
 
     private static RoomManager instance;
+    
     private List<Room> rooms;
 
     private RoomManager() {
